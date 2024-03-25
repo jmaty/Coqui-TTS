f7764e4f7f8118d2791f529886fb4500261413e7
Dan Tihelka <dtihelka@gmail.com>
2024-03-25 12:49:59 +0100

    Add sample loss implementations

--- TTS/hds/vits.py
+++ TTS/hds/vits.py
@@ -16,12 +16,13 @@ from TTS.utils.audio.processor import AudioProcessor
 class VitsHDS(Vits):
 
 
+    # --------------
     def __init__(self, config:Coqpit, ap:AudioProcessor = None, tokenizer:TTSTokenizer = None, **kws):
         """ Custom VITS constructor. Add additional parameters as required.
         """
         super().__init__(config, ap = ap, tokenizer = tokenizer)
 
-
+    # --------------
     def train_step(self, batch: dict, criterion: List[str], optimizer_idx: int) -> Tuple[Dict, Dict]:
         """Perform a single training step. Run the model forward pass and compute losses.
 
@@ -36,15 +37,84 @@ class VitsHDS(Vits):
 
         print('!!!! Volám DUMMY implementaci train_step() - vrací nesmyslnou hodnotu loss.')
 
-        # Build completely meaningless loss value
-        outputs = self.forward(batch["tokens"], batch["token_lens"], batch["spec"], batch["spec_lens"], batch["waveform"])
-        loss = torch.mean(outputs["model_outputs"])
 
-        # Get the minimum of the required data
-        return {'waveform_seg' : batch["waveform"],
-                'model_outputs' : batch["waveform"],
-                'alignments' : outputs["alignments"],
-               },\
-               {"loss" : loss,
-               }
+        # Discriminator
+        if optimizer_idx == 0 :
 
+           # Synthesize
+           # NOTE: may be cached for the future use
+           outputs = self.forward(batch["tokens"], batch["token_lens"], batch["spec"], batch["spec_lens"], batch["waveform"])
+
+           # Generated and real speech segments
+           speech_gener = outputs['model_outputs']
+           speech_real  = outputs['waveform_seg']
+
+           # Use discriminator
+           # Detach the generated speech, as we do not want update weights of the generator
+           scores_disc_fake,_,scores_disc_real,_ = self.disc(speech_gener.detach(), speech_real)
+
+           # Compute the loss. This loss will affect the (discriminator) DNN, as it uses data PROVIDED by it (outputs of the discriminator).
+           loss  = self.sample_loss1(scores_disc_fake, scores_disc_real)
+           loss *= 2 # A weight
+
+           # Get the minimum of the required data
+           return {'waveform_seg' : batch["waveform"],
+                   'model_outputs' : batch["waveform"],
+                   'alignments' : outputs["alignments"],
+                  },\
+                  {"loss" : loss,
+                  }
+
+
+        # Generator
+        else :
+
+           # Synthesize
+           outputs = self.forward(batch["tokens"], batch["token_lens"], batch["spec"], batch["spec_lens"], batch["waveform"])
+
+           # Generated and real speech segments
+           speech_gener = outputs['model_outputs']
+           speech_real  = outputs['waveform_seg']
+
+           # Compute the loss. This loss will affect the DNN, as it uses data PROVIDED by the DNN (generated speech samples).
+           loss  = self.sample_loss2(speech_real, speech_gener)
+           loss *= 5 # A weight
+
+           # Get the minimum of the required data
+           return {'waveform_seg' : batch["waveform"],
+                   'model_outputs' : batch["waveform"],
+                   'alignments' : outputs["alignments"],
+                  },\
+                  {"loss" : loss,
+                  }
+
+
+    # --------------
+    def sample_loss1(self, scores_disc_fake:torch.tensor, scores_disc_real:torch.tensor) -> float:
+        """Sample loss implementation. It computes
+
+        Args:
+            scores_disc_fake: the scores of the discriminator when recognising generated (fake) data
+            scores_disc_real: the scores of the discriminator when recognising real data
+
+        Returns:
+            Sum of absolute differences of fake/real scores
+        """
+        loss = 0
+        for sr,sf in zip(scores_disc_real, scores_disc_fake) :
+            loss += torch.mean(torch.abs(sf-sr))
+        return loss
+
+    # --------------
+    def sample_loss2(self, waveform_real:torch.tensor, waveform_gen:torch.tensor) -> float:
+        """Sample loss implementation. It computes MSE between real and generated samples
+
+        Args:
+            waveform_real: the real waweform segments
+            waveform_gen: the generated waweform segments
+
+        Returns:
+            RMS value between real and generated speech samples
+        """
+        # MSE (mean-square error)
+        return torch.mean((waveform_real-waveform_gen)**2)
